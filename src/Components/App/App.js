cdce026133b9bb8981d48d5534e941f045d42fc5
Bhupinder Singh <b.bains1986@gmail.com>
2022-06-21 17:35:44 +0100

    journal context added to object showing in console and delete button deleting

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import Input from '../Journal-Input/Journal-Input';
+import { useState } from 'react';
+
+
+
+
+  
 
 function App() {
+
+  const [ journal,setJournal] = useState([{
+    journalInformation:"",
+    author: "",
+    id: 1
+  }]); 
+
+  console.log(journal);
+
+function removeJournal(id) {
+
+const filterJournal = journal.filter((item)=>{
+
+if(item.id !== id){
+return item
+}
+
+})
+
+setJournal(filterJournal)
+}
+
   return (
     <div>
-    <Input/>
+    {journal.map((item)=>{
+      return<li key={item.id}><h1>{item.journalInformation}</h1><button onClick={()=>removeJournal(item.id)}>Delete</button></li>
+    })}
+    <Input setJournal={setJournal} journal={journal}/>
     </div>
   );
 }
--- src/Components/Journal-Input/Journal-Input.js
+++ src/Components/Journal-Input/Journal-Input.js
@@ -1,20 +1,32 @@
 import React from "react";
-import './Journal-Input.css';
+//import './Journal-Input.css';
+import { useState } from 'react';
 /*
-Pick the correct element to type into for our journal
-Then use state within the function below for a string
+Pick the correct element to type into for our journal..done
+Then use state within the function below for a string...already in text area content
 Then create a function to catch the string value
 */
 
-function Input( ) {
+
+function Input({setJournal, journal}) {
+
+  const [text, setText] = useState(" ");
+
+ function update (e) {
+ setText(e.target.value) //invoking not declaring!
+ }
+console.log(text);
 
     return(<div>
-           <textarea
-                //name="daily-entry"
-                //id="entry"
-                class="entry-text-box"
+          <textarea
+                name="daily-entry"
+                value={text}
+                className="entry-text-box"
                 placeholder="What's on your mind today? 💭"
+                onChange={update}
               ></textarea>
+              
+              <button onClick={()=>setJournal([...journal, {journalInformation:text, id: Math.floor(Math.random()*1000)}])}>Add Journal</button>
     </div> )
 }
 
