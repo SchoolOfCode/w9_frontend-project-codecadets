7334eb44be46b9cef41af2ff345ad6779054344a
Omar Labban <labban.omar@gmail.com>
2022-06-22 10:52:19 +0100

    a bit of styling added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,41 +1,44 @@
-import React from 'react';
-import Input from '../Journal-Input/Journal-Input';
-import { useState } from 'react';
-
-
-
-
-  
+import React from "react";
+import Input from "../Journal-Input/Journal-Input";
+import { useState } from "react";
 
 function App() {
-
-  const [ journal,setJournal] = useState([{
-    journalInformation:"",
-    author: "",
-    id: 1
-  }]); 
+  const [journal, setJournal] = useState([
+    {
+      journalInformation: "",
+      author: "",
+      id: 1,
+    },
+  ]);
 
   console.log(journal);
 
-function removeJournal(id) {
+  function removeJournal(id) {
+    const filterJournal = journal.filter((item) => {
+      if (item.id !== id) {
+        return item;
+      }
+    });
 
-const filterJournal = journal.filter((item)=>{
-
-if(item.id !== id){
-return item
-}
-
-})
-
-setJournal(filterJournal)
-}
+    setJournal(filterJournal);
+  }
 
   return (
     <div>
-    {journal.map((item)=>{
-      return<li key={item.id}><h1>{item.journalInformation}</h1><button onClick={()=>removeJournal(item.id)}>Delete</button></li>
-    })}
-    <Input setJournal={setJournal} journal={journal}/>
+      {journal.map((item) => {
+        return (
+          <li key={item.id}>
+            <h1>{item.journalInformation}</h1>
+            <button
+              class="btn-main entry-submit-btn"
+              onClick={() => removeJournal(item.id)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+      <Input setJournal={setJournal} journal={journal} />
     </div>
   );
 }
--- src/Components/Journal-Input/Journal-Input.js
+++ src/Components/Journal-Input/Journal-Input.js
@@ -1,38 +1,46 @@
 import React from "react";
-//import './Journal-Input.css';
-import { useState } from 'react';
+import "./Journal-Input.css";
+import { useState } from "react";
 /*
 Pick the correct element to type into for our journal..done
 Then use state within the function below for a string...already in text area content
 Then create a function to catch the string value
 */
 
-
-function Input({setJournal, journal}) {
-
+function Input({ setJournal, journal }) {
   const [text, setText] = useState(" ");
 
- function update (e) {
- setText(e.target.value) //invoking not declaring!
- }
-console.log(text);
+  function update(e) {
+    setText(e.target.value); //invoking not declaring!
+  }
+  console.log(text);
 
-    return(<div>
-          <textarea
-                name="daily-entry"
-                value={text}
-                className="entry-text-box"
-                placeholder="What's on your mind today? 💭"
-                onChange={update}
-              ></textarea>
-              
-              <button onClick={()=>setJournal([...journal, {journalInformation:text, id: Math.floor(Math.random()*1000)}])}>Add Journal</button>
-    </div> )
+  return (
+    <div>
+      <header>
+        <h1 class="title">My Personal Journal</h1>
+      </header>
+      <textarea
+        name="daily-entry"
+        value={text}
+        className="entry-text-box"
+        placeholder="What's on your mind today? 💭"
+        onChange={update}
+      ></textarea>
+
+      <button
+        class="btn-main entry-submit-btn"
+        onClick={() =>
+          setJournal([
+            ...journal,
+            { journalInformation: text, id: Math.floor(Math.random() * 1000) },
+          ])
+        }
+      >
+        Add to Journal
+      </button>
+    </div>
+  );
 }
 
 export default Input;
-
-
-
-
-
