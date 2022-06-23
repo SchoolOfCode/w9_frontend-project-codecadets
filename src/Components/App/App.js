5d02566c30c953fac97c503ecae09bd91e76a3e4
Bhupinder Singh <b.bains1986@gmail.com>
2022-06-23 13:53:49 +0100

    Back end and front end working!

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,29 +3,6 @@ import Input from "../Journal-Input/Journal-Input";
 import { useState, useEffect } from "react";
 
 
-/* use effect download at the top to use.@
-
-use the hook in the function above the component
-to use the hook we have to type useEffect () with a argument so with a dependenacy array
-
-useEffect (()=> { 
-console.log 'useEffect ran'; 
-}, []);
-
-
-what do we add to useEffect array as a state value to allow the use effect to monitor ? the state of a journal being posted and button to delete.
-
-JSON
-make a data folder in src level
-Make a db file called db.json in the data folder
-Make a fake journal from the use state we already have as a dummy
-
-
-
-
-*/
-
-
 
 function App() {
   const [journal, setJournal] = useState([]); //could this be null ? with a && condition
