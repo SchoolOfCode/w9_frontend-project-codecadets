8f605a87f351529dccb35875143be09e167cb5ea
nas <nasraxsaleh@hotmail.com>
2022-06-23 16:33:54 +0100

    table updates

--- src/Components/HomePage/Home/index.js
+++ src/Components/HomePage/Home/index.js
@@ -15,10 +15,13 @@ function Home() {
         <Quotes /> 
         </div>
         <section>
-        <div className="Square">
-      <img className="ImgsOne" src="https://ed.ted.com/teded-logo-1200-670.png" alt="TedEd"></img>
-      <img className="ImgsTwo" src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="Learning"></img>
-      <img className="ImgsThree" src="https://images.unsplash.com/photo-1607827448452-6fda561309d0?ixlib=rb-1.2.1&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80" alt="Worry less"></img>
+        <div className="wrapper row">
+      <img className="column" src="https://ed.ted.com/teded-logo-1200-670.png" alt="TedEd"></img>
+      <a href="https://www.youtube.com/watch?v=ZQUxL4Jm1Lo"> Link to video. </a>
+      <img className="column" src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="Learning" ></img>
+      <a href="https://www.freecodecamp.org/news/how-to-learn-programming/"> Link to article. </a>
+      <img className="column" src="https://images.unsplash.com/photo-1607827448452-6fda561309d0?ixlib=rb-1.2.1&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80" alt="Worry less" ></img>
+      <a href="https://positivepsychology.com/stress-management-techniques-tips-burn-out/"> Link to article. </a>
       </div>
 
         </section>
