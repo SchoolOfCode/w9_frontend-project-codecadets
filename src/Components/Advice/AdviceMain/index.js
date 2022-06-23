508a43bc4b8a656a9e3d0817e5652161464abe4c
nas <nasraxsaleh@hotmail.com>
2022-06-23 12:28:46 +0100

    webiste linked

---
+++ src/Components/Advice/AdviceMain/index.js
@@ -0,0 +1,16 @@
+import "./index.css";
+import Text from "../Text/text.js"
+
+
+
+function Advice() {
+    return (
+    <div>
+    <h1> Advice </h1>
+    <Text /> 
+    
+      </div>
+    )
+  };
+
+export default Advice;
