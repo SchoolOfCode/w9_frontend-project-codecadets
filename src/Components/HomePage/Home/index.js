9ef4d13e55433fc3f2fcd615276ab2ab9f0d3345
nas <nasraxsaleh@hotmail.com>
2022-06-23 23:37:05 +0100

    final homepage css done

--- src/Components/HomePage/Home/index.js
+++ src/Components/HomePage/Home/index.js
@@ -15,8 +15,11 @@ function Home() {
         </div>
         <section>
         <div className="wrapper row images">
+          <h4 className="centered-one box-images"> IMPOSTER SYNDROME </h4>
       <img className="column" src="https://static.psycom.net/wp-content/uploads/2020/06/iStock-1136737354.jpg" alt="TedEd"></img>
+      <h4 className="centered-two box-images"> OVERWHELMING CODE </h4>
       <img className="column" src="https://images-stylist.s3-eu-west-1.amazonaws.com/app/uploads/2022/02/07124103/managing-anxiety.jpg" alt="Learning" ></img>
+      <h4 className="centered-three box-images"> STRESSFUL TIMES </h4>
       <img className="column" src="https://static.psycom.net/wp-content/uploads/2020/07/iStock-1166335598.jpg" alt="Worry less" ></img>
       </div>
 
@@ -24,10 +27,10 @@ function Home() {
         <div class="grid-container">
 
     <div class="grid-child purple">
-       <p className="text-paragraph"> Do I belong here? Am I worthy? This is what a person dealing with 
+       <p className="text-paragraph"> 
+        Do I belong here? Am I worthy? This is what a person dealing with 
          Impostor syndrome may ask themselves. This internal experience of believing that you 
-        are not as competent is common and can be dealt with, watch the video below to 
-        learn more.
+        are not as competent is common and can be dealt with, watch the video below.
        <a href="https://www.youtube.com/watch?v=ZQUxL4Jm1Lo" className="link-text"> Link to video. </a> 
         </p>
 
@@ -35,8 +38,9 @@ function Home() {
     </div>
 
     <div class="grid-child green">
-    <p className="text-paragraph" > Learning to code can be overwhelming, with so many resources online you can fall into 
-    the trap of analysis paralysis or procrastinating. This article will assist you in your learning journey.
+    <p className="text-paragraph" > 
+    Learning to code can be overwhelming, with so many resources online you can fall into 
+    the trap of analysis paralysis or procrastinating. This article will assist and support you in your learning journey.
     <a href="https://www.freecodecamp.org/news/how-to-learn-programming/" className="link-text"> Link to article. </a>
      </p>
    
@@ -44,7 +48,7 @@ function Home() {
     
     <div class="grid-child green">
     <p className="text-paragraph"> While stress symptoms vary widely, odds are good that at some point you’ve experienced occasional physical and emotional 
-    distress signals. That's normal. This article is a great guide to manage stress.
+    distress signals. This article is a great guide to manage stress.
     <a href="https://positivepsychology.com/stress-management-techniques-tips-burn-out/" className="link-text"> Link to article. </a>
     </p>
     
--- src/Components/HomePage/Quotes/index.js
+++ src/Components/HomePage/Quotes/index.js
@@ -2,12 +2,12 @@ import "./index.css";
 import { useState} from "react";
 
 let words = ["I am in the right place at the right time, doing the right thing.", 
-"Courage starts with showing up and letting ourselves be seen.", "Make way for the unprecedented and watch your reality rearrange yourself.", "Am I good enough? Yes I am.", 
+"Courage starts with showing up and letting ourselves be seen.", "I am capable.", "Am I good enough? Yes I am.", 
 "The perfect moment is this one.", "Your perspective is unique. It's important and it counts.", "You must do the things you think you cannot do.", "I am worthy."]
 
 
 function Quotes() {
-const [quote, setQuote] = useState(["Press button to generate a positive quote."]);
+const [quote, setQuote] = useState(["Press the button below to generate a positive quote."]);
 const handleClick = (event) => {
 setQuote(words[Math.floor(Math.random()*words.length)]);
 }
