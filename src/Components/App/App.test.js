c4f7d3321294e3059f58979a04d94477535196a9
megantyson <102684495+megantyson@users.noreply.github.com>
2022-06-29 12:34:14 +0100

    testing in progress

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders journal page link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Journal/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders about page link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/About/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders home page link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders advice page link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Advice/i);
   expect(linkElement).toBeInTheDocument();
 });
